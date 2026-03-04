20cf20edf4a3908d3ebb93b85dd674cf27cc00f5
Diego Sanchez Franco <diego.sanchez@ivaq.es>
2026-03-04 14:56:03 +0100

    back to using 1Hz for homepoint altitude

--- psdk_wrapper/src/modules/telemetry.cpp
+++ psdk_wrapper/src/modules/telemetry.cpp
@@ -2542,7 +2542,7 @@ TelemetryModule::subscribe_psdk_topics()
     }
     return_code = DjiFcSubscription_SubscribeTopic(
         DJI_FC_SUBSCRIPTION_TOPIC_ALTITUDE_OF_HOMEPOINT,
-        get_frequency(params_.control_information_frequency),
+        DJI_DATA_SUBSCRIPTION_TOPIC_1_HZ,
         c_home_point_altitude_callback);
 
     if (return_code != DJI_ERROR_SYSTEM_MODULE_CODE_SUCCESS)
